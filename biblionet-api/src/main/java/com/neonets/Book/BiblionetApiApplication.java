9120d22d5f29f63b6635cc16f71d1eb84de7e58d
proneon <pntombelafirst@gmail.com>
2025-07-19 19:07:58 +0200

    Changed application.xml to a yml. created a new docker-compose.yml file to configure my services.

---
+++ biblionet-api/src/main/java/com/neonets/Book/BiblionetApiApplication.java
@@ -0,0 +1,13 @@
+package com.neonets.Book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblionetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblionetApiApplication.class, args);
+	}
+
+}
---
+++ biblionet-api/src/test/java/com/neonets/Book/BiblionetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neonets.Book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblionetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
